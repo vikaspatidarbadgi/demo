db9a9a29c429db9529cfff62b9a4af4a2c5a2fb0
vikas patidar <50407553+vikaspatidarbadgi@users.noreply.github.com>
2022-05-23 14:22:34 -0700

    my first project

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// let menu = document.querySelector(".bars_icon");
+// menu.onclick=function(){
+//    let main_menu = document.querySelector(".nav_main_div");
+//    if(main_menu.offsetHeight == 50){
+//        main_menu.style.transition="0.3s";
+//        main_menu.style.height="300px";
+//        main_menu.style.boxShadow="0px 3px 3px #ddd";
+//    }
+//    else{
+//        main_menu.style.transition="0.3s";
+//        main_menu.style.height="50px"; 
+//        main_menu.style.boxShadow="none";
+     
+
+
+//    }
+// }
+
+// setTimeout check
